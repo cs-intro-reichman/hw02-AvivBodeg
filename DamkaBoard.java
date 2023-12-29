ae74ee2683ccea4f145d1d24cde16b59e1e2fce3
Aviv Bodeg <AVIVBodeg@gmail.com>
2023-12-29 20:31:15 +0200

    questions 1 - 5

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,8 +1,23 @@
 /**
- *  Gets a command-line argument n (int), and prints an n-by-n damka board.
+ * Gets a command-line argument n (int), and prints an n-by-n damka board.
  */
 public class DamkaBoard {
-	public static void main(String[] args) {
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        int size = Integer.parseInt(args[0]);
+
+        String evenRow = "";
+        String oddRow = "";
+
+        for (int i = 0; i < size; i++) {
+            oddRow += "* ";
+            evenRow += " *";
+        }
+        for (int i = 1; i <= size; i++) {
+            if (i % 2 == 0) {
+                System.out.println(evenRow);
+            } else {
+                System.out.println(oddRow);
+            }
+        }
+    }
 }
--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,16 @@
  * The program expects to get one command-line argument: A string.
  */
 public class Reverse {
-	public static void main (String[] args){
-		//// Put your code here
-	}
+    public static void main(String[] args) {
+        String str = args[0];
+        String reversed = "";
+        int len = str.length();
+
+        for (int i = 0; i < len; i++) {
+            reversed = str.charAt(i) + reversed;
+        }
+
+        System.out.println(reversed);
+        System.out.println("The middle character is " + str.charAt(len / 2));
+    }
 }
